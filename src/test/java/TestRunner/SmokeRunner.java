9e948713223e499cac057cdf83e16a60b2904bbd
lzgircha <lzgircha@gmail.com>
2023-04-04 22:12:11 -0400

    AddEmployee

--- src/test/java/TestRunner/SmokeRunner.java
+++ src/test/java/TestRunner/SmokeRunner.java
@@ -6,6 +6,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Features", glue = "StepDefinitions", dryRun = false)
+@CucumberOptions(features = "src/test/resources/Features",
+        glue = "StepDefinitions",
+        dryRun = false,
+        tags = "@testcase2")
+//if you want to run two test cases at the same time use "or" Ex. tags = "@testcase1 ot @testcase2"
+//through tags you can combine test cases
+
 public class SmokeRunner {
 }
+//tags option will execute the tagged testcases as mentioned in your runner class
